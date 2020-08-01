c9e3022d3dd26dd57e86ad27a5c64971850bee99
iampratiktandel <iampratiktandel@gmail.com>
2020-08-01 13:50:02 +0530

    Story Part 2

--- story_start.js
+++ story_start.js
@@ -8,5 +8,19 @@ let lightningStart = {"left": "290px"};
 let lightningEnd = {"left": "900px"};
 let options = {"duration": 1000};
 
+
+let hitTheVillain = () => {
+  let villainStart = {
+    transform: 'rotate(0deg)',
+    opacity: 100
+  };
+  let villainEnd = {
+    transform: 'rotate(1000deg)',
+    opacity: 0 
+  };
+  let options = {"duration": 1500};
+  villain.animate([villainStart, villainEnd], options);
+}
+
 //Run animation code here
-lightning.animate([lightningStart,lightningEnd], options);
+lightning.animate([lightningStart,lightningEnd], options).onfinish = hitTheVillain;
